6f74a95460bd3ab3f32016a136ff5845c91da675
shorya-sharma97 <shoryeps4@gmail.com>
2026-03-11 11:01:26 +0530

    Coded UC1: Display Hello World in console

---
+++ helloapp/HelloAppUC1.java
@@ -0,0 +1,11 @@
+/**
+ * HelloApp serves as the entry point for UC1.
+ * It simply prints "Hello World" to the console.
+ */
+public class HelloAppUC1 {
+
+    public static void main(String[] args) {
+        // Output the literal string to the console
+        System.out.println("Hello World");
+    }
+}
