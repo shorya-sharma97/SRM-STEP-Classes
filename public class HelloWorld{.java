f46fec318c4ee6cfbdc07a5bc22db953be985a96
shorya-sharma97 <shoryeps4@gmail.com>
2026-03-11 08:51:33 +0530

    Initial commit

---
+++ public class HelloWorld{.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp displays the OOPS banner in various formats.
+ * UC1: Prints the literal string "OOPS" to the console.
+ * * @author User
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The entry point of the application.
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        // Output the literal string to the console
+        System.out.println("OOPS");
+    }
+}
