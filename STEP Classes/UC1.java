3a3559902b0d454e399994afb257f113cf41e154
shorya-sharma97 <shoryeps4@gmail.com>
2026-03-11 09:39:32 +0530

    Added UC1 Java program and gitignore

---
+++ STEP Classes/UC1.java
@@ -0,0 +1,17 @@
+/**
+ * UC1 displays the OOPS banner in various formats.
+ * UC1: Prints the literal string "OOPS" to the console.
+ * * @author User
+ * @version 1.0
+ */
+public class UC1 {
+
+    /**
+     * The entry point of the application.
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        // Output the literal string to the console
+        System.out.println("OOPS");
+    }
+}
