f338c3e59397f762cfcaa0408eab5ae342aab0f5
Shorya Sharma <shoryeps4@gmail.com>
2026-03-18 22:02:04 +0530

    feat: Implement UC2 - Display Hello with command-line argument

---
+++ STEP Classes/HelloApp.java
@@ -0,0 +1,14 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        String name;
+        
+        // Check if command-line argument exists
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+        
+        System.out.println("Hello, " + name + "!");
+    }
+}
